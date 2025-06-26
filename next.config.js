ba921a7c27dbc56ac18e5f5b92c2846f500bf15e
rangelbruno <brunojp.rangel@gmail.com>
2025-06-26 10:44:04 -0300

    fix: convert next.config to CommonJS for Node18

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   eslint: {
     // Ignorar erros de ESLint durante o build de produção
     ignoreDuringBuilds: true,
   },
-}
-
-export default nextConfig; 
+}; 
